1d3aaacb74c01b333df51e2ed01bcf1495d38eac
Shivang Goswami <shivang.goswami1@ibm.com>
2023-01-30 15:08:49 +0530

    message code

---
+++ async-time-service/messageService/message.go
@@ -0,0 +1,32 @@
+package message
+
+import (
+	"math/rand"
+	"time"
+)
+
+var (
+	messages = []string{
+		"Time is money",
+		"It's time for a beer",
+		"It's time for two beers! Or three!",
+		"Time it's relative",
+		"Good time to ride a bike",
+		"So much time",
+		"It's five o'clock somewhere",
+		"Time to relax",
+		"It's time to get out of here",
+	}
+	lengthofmessages = len(messages) + 1
+)
+
+var rdgen *rand.Rand
+
+func init() {
+	seed := rand.NewSource(time.Now().UnixNano())
+	rdgen = rand.New(seed)
+}
+
+func GetMessage() string {
+	return messages[rdgen.Intn(lengthofmessages)]
+}
